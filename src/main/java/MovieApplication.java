77ac61593bbdf22cdad1ff78b101f1db275fc614
conatuseus <conatuseus@gmail.com>
2019-04-16 15:33:29 +0900

    Add method for calculating price

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -1,11 +1,16 @@
 import domain.Movie;
 import domain.MovieRepository;
+import domain.Reservation;
+import domain.User;
 import view.InputView;
 import view.OutputView;
 
 import java.util.List;
 
 public class MovieApplication {
+
+    User user;
+    Reservation reservation;
     public static void main(String[] args) {
         List<Movie> movies = MovieRepository.getMovies();
         OutputView.printMovies(movies);
--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -5,8 +5,15 @@ import java.util.List;
 
 public class User {
 
-    private List<Reservation> reservationList=new ArrayList<>();
+    private final List<Reservation> reservationList=new ArrayList<>();
 
 
+    public int sumOfPrice(){
+        int sumOfPrice=0;
+        for(Reservation reservation : reservationList){
+            sumOfPrice+= reservation.price();
+        }
+        return sumOfPrice;
+    }
 
 }
